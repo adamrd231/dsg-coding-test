6d6ca11311418c1eac9733674d78c1f90b4a5fa6
adamrd231 <adam.rdconcepts@gmail.com>
2022-03-14 12:34:41 -0400

    Initial Commit

---
+++ DSG-code-test/DSG_code_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSG_code_testApp.swift
+//  DSG-code-test
+//
+//  Created by Adam Reed on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct DSG_code_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
